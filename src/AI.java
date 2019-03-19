1fb85a549a3cd4f738624dfcff787127f60c8bbe
Hoegsgaard <mathiashoegsgaard@gmail.com>
2019-03-19 09:35:25 +0100

    AI UpComming!

--- src/AI.java
+++ src/AI.java
@@ -1,18 +1,37 @@
 public class AI {
-    int score = 0;
-    int[] savedKalahaBoard = new int[14];
+    int[] board = new int[14];
+    int[][] heap;
+    boolean goalState = board[13] < board[6];
 
+    public int alphaBeta(int[][] node, int depth, int alpha,int beta, boolean maxPlayer, int[] currentBoard){
+        int val;
+        setBoard(currentBoard);
+        if(depth == 0 || goalState){
+            if(goalState){
+                return (board[6] - board[13])*10;
+            } else if(depth == 0){
+                return evaluate();
+            }
+        }
+        if(maxPlayer){
+            val = -1000000;
 
-
-    public void bestMove(){
-
-
-
+        } else{
+            val = 1000000;
+        }
+        return val;
     }
 
-    public void savedBoard(){
-        for(int i = 0; i < savedKalahaBoard.length; i++){
-            
+
+    public int evaluate(){
+
+        return 1; // FEJL
+    }
+
+
+    public void setBoard(int[] currentBoard){
+        for(int i = 0; i < board.length; i++){
+            board[i] = currentBoard[i];
         }
     }
 }
